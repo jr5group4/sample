d9cb6cca612a636424bf388b5790c9fe6d30fdac
Pavithra <Pavithra@DESKTOP-J0CQQ4T>
2021-03-27 13:41:45 +0530

    add sample

---
+++ sample-educ/src/main/java/com/cg/sample/appln/Sample.java
@@ -0,0 +1,12 @@
+package com.cg.sample.appln;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
